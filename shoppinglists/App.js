cf301d466b185651a0b4536092ef97aa6cdae3b1
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-05 21:07:06 +0000

    build: initialize Firebase and Firestore

--- shoppinglists/App.js
+++ shoppinglists/App.js
@@ -1,7 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
 
-export default function App() {
+const App = () => {
+  // app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBg5jzJn7POPBohuVoACW5z_dK4ae5f-9k",
+    authDomain: "shopping-list-demo-caf2c.firebaseapp.com",
+    projectId: "shopping-list-demo-caf2c",
+    storageBucket: "shopping-list-demo-caf2c.appspot.com",
+    messagingSenderId: "200436510215",
+    appId: "1:200436510215:web:3ce47177af9dcc4aff84a2",
+  };
+
+  // initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // initialize Cloud Firestore 
+  const db = getFirestore(app);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +31,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
